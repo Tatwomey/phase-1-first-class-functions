0f389c61362447f982987c62d21cf889e8df0721
Trevor Twomey <trevor.a.twomey@gmail.com>
2022-12-13 13:53:14 -0500

    commit tree 271bf788dcb0ef449f44e6bde8b5ded7ae539d91

---
+++ index.js
@@ -0,0 +1,15 @@
+ function spy() {return "hello world"
+}
+
+function receivesAFunction(spy) {
+    return spy();
+}
+
+function returnsANamedFunction(){
+return spy ()
+}
+
+
+
+returnsANamedFunction("Hello world")
+returnsANamedFunction()
